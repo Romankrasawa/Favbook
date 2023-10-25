ef0b4da589cfdba27bcae42e3fcada745bd4dfd4
Romankrasawa <romankrasawa8567@gmail.com>
2023-10-25 23:20:31 +0300

    env files

--- book_forum/config/settings.py
+++ book_forum/config/settings.py
@@ -9,7 +9,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 load_dotenv(dotenv_path=str(BASE_DIR.parent) + "/" + os.environ.get("ENV_FILE"))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
-
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
